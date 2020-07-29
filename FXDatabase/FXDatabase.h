9673ad3341f2aa67a4bf3d1e0b467cb8ffebb123
wupengfei <1569485690@qq.com>
2020-07-29 19:58:22 +0800

    Initial Commit

---
+++ FXDatabase/FXDatabase.h
@@ -0,0 +1,19 @@
+//
+//  FXDatabase.h
+//  FXDatabase
+//
+//  Created by Mr.wu on 2020/7/29.
+//  Copyright © 2020 Mr.wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FXDatabase.
+FOUNDATION_EXPORT double FXDatabaseVersionNumber;
+
+//! Project version string for FXDatabase.
+FOUNDATION_EXPORT const unsigned char FXDatabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FXDatabase/PublicHeader.h>
+
+
